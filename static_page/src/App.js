3c86ea1c67bc47f36b2281b00c651d0527276a37
Millie Muteti <milliemuteti@gmail.com>
2023-06-20 16:27:02 +0300

    add components

--- static_page/src/App.js
+++ static_page/src/App.js
@@ -1,10 +1,12 @@
+import Navbar from './components/Navbar';
+import Main from './components/Main';
 
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className="container">
+        <Navbar />
+        <Main />
     </div>
   );
 }
